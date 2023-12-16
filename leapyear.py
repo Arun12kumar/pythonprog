e9b2e4d966f21d2e746390e6bd3df0f1057126d5
Arun kumar <arunakhil978@gmail.com>
2023-12-16 21:35:23 +0530

    commit message

---
+++ leapyear.py
@@ -0,0 +1,10 @@
+
+num = int(input("Enter a year:"))
+
+num1 = num % 4;
+
+if num1 == 0:
+    print( num, " is a leap year")
+
+else:
+    print(num, "not a leap year")
---
+++ odd.py
@@ -0,0 +1,9 @@
+
+num = int(input("Enter a number:"))
+
+x = num%2;
+
+if(x == 0):
+    print(num ,"is even")
+else:
+    print(num,"is odd")    
